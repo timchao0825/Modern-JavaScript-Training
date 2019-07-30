ae95af544428033da97a1fc958e4632220bbbb36
Tim <tim.chao0825@gmail.com>
2019-07-30 23:55:38 +0800

    no message

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,17 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores , roundScore , activePlayer , dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+// 輸出 1 ~ 6的變數
+// dice = 6;
+// math.randon() these are decimals
+// math.floor() are trans integer 
+// 函式會回傳無條件捨去後的最大整數。
+// Answer
+// Math.floor(Math.random() * 6 ) + 1
+dice = Math.floor(Math.random() * 6 ) + 1;
+console.log(dice);
