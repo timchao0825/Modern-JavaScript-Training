c60233eb9380666c54124ba578b51b8ef5ae64a2
Tim <tim.chao0825@gmail.com>
2019-08-12 23:57:26 +0800

    Section 4: 50class update

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -20,11 +20,40 @@ activePlayer = 0;
 // 函式會回傳無條件捨去後的最大整數。
 // Answer
 // Math.floor(Math.random() * 6 ) + 1
-dice = Math.floor(Math.random() * 6 ) + 1;
-// console.log(dice);
+dice = Math.floor(Math.random() * 6 ) + 1;// console.log(dice);
 document.querySelector('#current-'+activePlayer).textContent = dice;
 // document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+dice+'</em>';
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display the result
+  var diceDom = document.querySelector('.dice');
+  diceDom.style.display = 'block';
+  diceDom.src = 'dice-' + dice + '.png';
+  // 3. Update the round scroe if the rolled number was not
+  if(dice !== 1){
+    roundScore += dice;
+    document.querySelector('#current-'+activePlayer).textContent = roundScore;
+  }else{
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.add('active');
+
+  }
+})
